47bf62f251033e231878f6dd8ce53c13cf78eb97
uryone <racast324>
2020-12-06 12:26:19 +0900

    Add initial state slice for UI Display

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import issuesDisplaySlice from 'features/issuesDisplay/issuesDisplaySlice'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+  issuesDisplay: issuesDisplaySlice
+})
 
 export type RootState = ReturnType<typeof rootReducer>
 
